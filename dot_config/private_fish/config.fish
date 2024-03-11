01d44c10b47c376938c69c550d6616641ab6ec46
Matthew Bratrsovsky <realzing@pm.me>
2024-03-11 09:54:23 -0600

    up

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,5 +2,7 @@ if status is-interactive
     zoxide init fish | source
     alias cd="z"
     alias cat="bat"
+    alias nvim="$HOME/.apps/nvim.appimage"
+    alias q="exit"
     # Commands to run in interactive sessions can go here
 end
