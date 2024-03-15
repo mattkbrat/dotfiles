0b69b2474618400f66e7c17cfd9b21464d028094
Matthew Bratrsovsky <realzing@pm.me>
2024-03-15 09:40:04 -0600

    Up

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,7 +2,10 @@ if status is-interactive
     zoxide init fish | source
     alias cd="z"
     alias cat="bat"
+    alias nvim="$HOME/.apps/nvim.appimage"
     alias q="exit"
+    alias ls="eza"
+    set MANPAGER "sh -c 'col -bx | bat -l man -p'"
     set fish_user_paths /usr/local/bin $fish_user_paths
     set fish_user_paths $HOME/.apps $fish_user_paths
     # Commands to run in interactive sessions can go here
