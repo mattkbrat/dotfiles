e21adc21bd3fd3db5277e91342351b533429f908
Matthew Bratrsovsky <git.twisted254@slmail.me>
2024-03-15 19:08:23 -0600

    up

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,9 +1,11 @@
 if status is-interactive
     zoxide init fish | source
+    fzf --fish | source
     alias cd="z"
     alias cat="bat"
     alias q="exit"
     set fish_user_paths /usr/local/bin $fish_user_paths
     set fish_user_paths $HOME/.apps $fish_user_paths
+    set GPG_TTY $(tty)
     # Commands to run in interactive sessions can go here
 end
