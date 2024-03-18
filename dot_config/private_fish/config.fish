ed2984a95b0cd706da7f2da81f32f6e29dd70d40
Matthew Bratrsovsky <realzing@pm.me>
2024-03-18 08:43:47 -0600

    up

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -6,9 +6,25 @@ if status is-interactive
     alias nvim="$HOME/.apps/nvim.appimage"
     alias q="exit"
     alias ls="eza"
+    sh nvm
     set MANPAGER "sh -c 'col -bx | bat -l man -p'"
     set fish_user_paths /usr/local/bin $fish_user_paths
     set fish_user_paths $HOME/.apps $fish_user_paths
     set GPG_TTY $(tty)
+
+
+    function !!
+        set var (history | head -n 1)
+        if test $argv
+            if test $argv = sudo #; or "any other command you want to prepend"
+                eval $argv $var
+            else
+                eval $var $argv
+            end
+        else
+            eval $var
+        end
+    end
+
     # Commands to run in interactive sessions can go here
 end
