77f7bea0abfe845e948b36c6e540000687520d44
Matthew Bratrsovsky <realzing@pm.me>
2024-04-05 10:54:18 -0600

    up

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -45,3 +45,10 @@ set --export PATH $BUN_INSTALL/bin $PATH
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+# pnpm
+set -gx PNPM_HOME "/home/matt/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
