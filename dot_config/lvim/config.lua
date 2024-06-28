f2c1db0da443d43b5869824029d4b722f2d82df5
Matthew Bratrsovsky <realzing@pm.me>
2024-06-28 15:55:12 -0600

    lvim

---
+++ dot_config/lvim/config.lua
@@ -0,0 +1,22 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+lvim.plugins = {
+  { "lunarvim/colorschemes" },
+  {
+    "nvim-neorg/neorg",
+    ft = "norg", -- lazy-load on filetype
+    config = true, -- run require("neorg").setup()
+  },
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+}
+
+lvim.keys.normal_mode["-"] = ":Oil<CR>"
