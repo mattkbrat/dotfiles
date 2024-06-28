06cce3d5d93001fc1da28bd91a1239fffe811793
Matthew Bratrsovsky <realzing@pm.me>
2024-06-28 10:00:09 -0600

    luarocks

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+
+vim.g.python_host_prog = "~/.config/python3-venv/bin/python"
+vim.g.python3_host_prog = "~/.config/python3-venv/bin/python3"
+
 require("config.lazy")
