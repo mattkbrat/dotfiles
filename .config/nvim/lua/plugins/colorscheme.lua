f154a3341c86cff569fb7fa62ba56f475ee4d117
Matthew Bratrsovsky <realzing@pm.me>
2026-02-13 16:50:22 -0700

    chore(nvim): up

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,12 @@
 return {
   -- add gruvbox
-  -- { "ellisonleao/gruvbox.nvim" },
+  { "ellisonleao/gruvbox.nvim" },
 
-  { "jacoborus/tender.vim", "haystackandroid/snow" },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tender",
+      colorscheme = "gruvbox",
     },
   },
 }
